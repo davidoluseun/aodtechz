e286d564c238fd2742fb1a4d9d4d1c1444af16b8
David Oluseun <a.davidoluseun@gmail.com>
2020-06-04 22:40:04 +0100

    Adds project files

---
+++ js/main.js
@@ -0,0 +1,73 @@
+(function() {
+    "use strict";
+
+    $(document).ready(function() {
+
+        // Preloader animation
+        $(".preloader").fadeOut();
+
+        // Grab .nav-toggler element
+        var $navToggler = $(".nav-toggler");
+
+        // Attach click event to $navToggler
+        $navToggler.click(function() {
+
+            // Toggle .site-nav element on and off
+            $(".site-nav").toggleClass("show");
+
+            // Alternate $navToggler aria-expanded attribute value
+            if ($navToggler.attr("aria-expanded") == "false") {
+                $navToggler.attr("aria-expanded", "true");
+            } else {
+                $navToggler.attr("aria-expanded", "false");
+            }
+
+        });
+
+        // Validate newsletter forms on keyup and submit
+        $(".newsletter-form").validate({
+
+            // Set form validation rules
+            rules: {
+                newsletteremail: {
+                    required: true,
+                    email: true
+                }
+            },
+
+            // Set validation error messages
+            messages: {
+                newsletteremail: {
+                    required: "Please enter your email address.",
+                    email: "Please enter a valid email address."
+                }
+            },
+
+            // Set error messages element
+            errorElement: "div",
+
+            // Set placement position for error messages element
+            errorPlacement: function (error, element) {
+                // Check for element name attribute value
+                if (element.prop("name") === "newsletteremail") {
+                    error.insertAfter(element.next("button"));
+                } else {
+                    error.insertAfter(element);
+                }
+            },
+
+            // Highlight error element when input is invalid after submission attempt
+            highlight: function (element, errorClass, validClass) {
+					       $(element).addClass("is-invalid").removeClass("is-valid");
+				    },
+
+            // Highlight error element when input is valid after submission attempt
+				    unhighlight: function (element, errorClass, validClass) {
+					       $(element).addClass("is-valid").removeClass("is-invalid");
+				    }
+
+        }); // jQuery validation function
+
+    }); // jQuery document ready function
+
+})(); // Immediately Invoked Function Expression (IIFE)
